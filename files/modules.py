e8c0d632e683b4b728155b8941cd777bba7e3c4d
hex <hex@tzc888.com>
2020-09-30 14:24:44 +0800

    #优化代码

--- files/modules.py
+++ files/modules.py
@@ -48,6 +48,8 @@ def token_symbol(tokenname):
         return "wBTC"
     if tokenname == "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643":
         return "cDAI"
+    if tokenname == "0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9":
+        return "cUSDT"
 
 
 def api():
