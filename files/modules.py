ca0618df2e895d0ac6aaffd314db36ff41ab68d4
hex <hex@tzc888.com>
2020-09-30 09:45:49 +0800

    #优化代码

--- files/modules.py
+++ files/modules.py
@@ -55,15 +55,8 @@ def api():
         "page_size": 20,
         "min_borrow_value_in_eth": {"value": "0.2"}
     }
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
     try:
-        req = requests.get(site, params=params, headers=headers)
+        req = requests.get(site, data=json.dumps(params))
         response = req.json()
     except Exception as e:
         response = []
